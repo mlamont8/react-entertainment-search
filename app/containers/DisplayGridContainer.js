6bbaa5250dbc3c375c800c3a5d71a7aa75f96b9f
M L Windows <debiandmiat@hotmail.com>
2017-02-28 21:17:59 -0500

    id being sent

--- app/containers/DisplayGridContainer.js
+++ app/containers/DisplayGridContainer.js
@@ -1,25 +1,44 @@
 var React = require('react')
 var DisplayPosters = require('../components/explore/DisplayPosters')
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 var DisplayGridContainer = React.createClass({
 
-render: function(){
-  // return(
-  //   // <div className="row">
-  //   //
-  //   //     {this.props.data.map(data =>
-  //   //     <div key ={data.id} className='col s6 m2'>
-  //   //       <DisplayPosters data={data} type={this.props.type}/>
-  //   //     </div>)}
-  //   // </div>
-  //
-  //
-  // )
+  onItemClick: function(item, e){
+    console.log(item);
+  },
 
-  var items = this.props.data;
-      var _this = this;
-  return (
-    <div>
+render: function(){
+  return(
+    <div className="row">
+
+        {this.props.data.map(data => {
+              //bind components to onItemClick method
+              //Use bind to attach item argument
+              let boundItemClick = this.onItemClick.bind(this.data);
+          return (
+            <Link to={'/detail'}>
+              <div key ={data.id} onClick={boundItemClick} className='col s6 m2'>
+                <DisplayPosters data={data} type={this.props.type}/>
+              </div>
+            </Link>
+    )
+  }
+      )}
+    </div>
+
+
+  )
+}
+})
+
+
+
+  // var items = this.props.data;
+  //     var _this = this;
+  // return (
+  //   <div>
     // {() => {
     //   items.map((item) => {
     //     //bind components to onItemClick method
@@ -34,29 +53,29 @@ render: function(){
 
 
 
-    function(){
-        items.map(function (item) {
-            //bind components to onItemClick method
-            //Use bind to attach item argument
-            var boundItemClick = _this.onItemClick.bind(_this.item);
-            //Construct onClick with bound function
-            return key;
-            {
-                data.id;
-            }
-          <img src = { 'https://image.tmdb.org/t/p/w154' +_this.props.data.poster_path } />
-            ;
-        });
-    }
-    ()
+//     function(){
+//         items.map(function (item) {
+//             //bind components to onItemClick method
+//             //Use bind to attach item argument
+//             var boundItemClick = _this.onItemClick.bind(_this.item);
+//             //Construct onClick with bound function
+//             return key;
+//             {
+//                 data.id;
+//             }
+//           <img src = { 'https://image.tmdb.org/t/p/w154' +_this.props.data.poster_path } />
+//             ;
+//         });
+//     }
+//     ()
+//
+//   )
+// }
+// </div>,
+// onItemClick: function(item, e){
+//   console.log(item);
+// }
 
-  )
-}
-</div>,
-onItemClick: function(item, e){
-  console.log(item);
-}
-
-})
+//})
 
 module.exports = DisplayGridContainer;
