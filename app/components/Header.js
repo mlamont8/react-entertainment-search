41043df46593a66c938eb7915daf7147a91e8dc4
mlamont8 <debiandmiat@hotmail.com>
2017-03-13 15:57:35 -0400

    added react burger menu

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,6 +3,9 @@ var styles = require('../styles');
 var ReactRouter = require('react-router');
 var Search = require('./explore/Search')
 var Link = ReactRouter.Link;
+var Radium = require('radium')
+var RadiumLink = Radium(Link)
+var Menu = require('react-burger-menu').stack
 
 var Header = React.createClass({
 
@@ -18,7 +21,7 @@ getInitialState() {
 <header style={styles.header}>
 <nav className="transparent" style={styles.navStyles}>
   <div className="nav-wrapper">
-  
+
 {/* <!--Full sized Nav  --> */}
     <ul id="nav-full" className="right hide-on-med-and-down white-text">
       <li><Link className="white-text" activeClassName="active" to="/" style={styles.headerFont}>Home</Link></li>
@@ -26,15 +29,23 @@ getInitialState() {
       <li><Link className="white-text" activeClassName="active" to="/explore/tv"  style={styles.headerFont}>TV Shows</Link></li>
      </ul>
      {/* <!-- Mobile Button --> */}
-        <a href="#" className="button-collapse" data-activates="mobile-nav" data-sidenav="left" data-closeonclick="true" data-menuwidth="100">
-           <i className="material-icons">menu</i>
-         </a>
+
 {/* <!-- Mobile Sidenav Menu --> */}
-   <ul id="mobile-nav" className="black-text side-nav">
+   {/* <ul id="mobile-nav" className="black-text side-nav">
       <li><Link className="black-text" activeClassName="active" to="/">Home</Link></li>
       <li><Link  className="black-text" activeClassName="active" to="/explore/movie">Movies</Link></li>
      <li><Link className="black-text" activeClassName="active" to="/explore/tv">TV Shows</Link></li>
-     </ul>
+     </ul> */}
+
+     <Menu width={ 120 } styles={ styles } >
+       <RadiumLink className="menu-item" to="/">Home</RadiumLink>
+       <RadiumLink className="menu-item" to="/explore/movie">Movies</RadiumLink>
+       <RadiumLink className="menu-item" to="/explore/tv">TV Shows</RadiumLink>
+       {/* <RadiumLink onClick={ this.showSettings } className="menu-item--small" href="">Settings</RadiumLink> */}
+       {/* <RadiumLink onClick={ this.showSettings } href="#" className=" menu-item button-collapse"  data-sidenav="left" data-closeonclick="true">
+          <i className="material-icons">menu</i>
+        </RadiumLink> */}
+     </Menu>
 
   </div>
 </nav>
