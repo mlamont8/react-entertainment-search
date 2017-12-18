d490198ccac2a0267f0f5e8fba9a7f16ca800e2b
mlamont8 <mlamont8@hotmail.com>
2017-12-17 21:46:50 -0500

    updated main title

--- app/components/home/MainDivider.js
+++ app/components/home/MainDivider.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Col, Row} from 'react-bootstrap';
+import {Col, Row, Button} from 'react-bootstrap';
+import { Link } from 'react-router';
 
 
 class MainDivider extends React.Component {
@@ -7,11 +8,22 @@ class MainDivider extends React.Component {
   render() {
     return (
       <Row>
-        <Col className="col red darken-4 white-text titleBlock" xs={12} >
+
+        <Row>
+        <Col className="white-text text-center titleBlock" xs={12} >
           <p className="mainDividerTitle">
               Whats Out
           </p>
       </Col>
+      </Row>
+
+      <Row>
+        <Col className="text-center subTitle" xs={12}>
+          <span><Link to="/explore/movie">Hot Movies</Link></span>
+          <span><Link to="/explore/tv">Hot TV Shows</Link></span>
+        </Col>
+        </Row>
+
     </Row>
     );
   }
