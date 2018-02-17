313b13922ba84322f4136c3d3e6699afa84499c8
mlamont8 <debiandmiat@hotmail.com>
2018-02-17 13:16:48 -0500

    displayGridcontainer refactor

--- app/containers/DisplayGridContainer.js
+++ app/containers/DisplayGridContainer.js
@@ -5,34 +5,25 @@ import ReactRouter, {Link} from 'react-router';
 import {Col, Row, Image} from 'react-bootstrap';
 
 
-class DisplayGridContainer extends React.Component {
-
-  render() {
-    return (
-      <Row className="row">
-
-          {this.props.data.map(data => {
-              return (
-
-                <Col key={data.id} lg={2} sm={4} xs={6} className=' gridCol' >
-
-                  <Link to={'detail/'+ this.props.type +'/'+ data.id } >
-                    <Image responsive src = {'https://image.tmdb.org/t/p/w154' + data.poster_path}
-                   />
-                  </Link>
-
-                </Col>
-
-              )
-            }
-        )}
-      </Row>
+const DisplayGridContainer = ({data,type}) => {
+        return (
+          <Row className="row">
+              {data.map(data => {
+                  return (
+                    <Col key={data.id} lg={2} sm={4} xs={6} className=' gridCol' >
+                      <Link to={'detail/'+ type +'/'+ data.id } >
+                        <Image responsive src = {'https://image.tmdb.org/t/p/w154' + data.poster_path}
+                       />
+                      </Link>
+                    </Col>
+                  )
+                }
+            )}
+          </Row>
+        );
+      }
 
 
-    );
-  }
-
-}
 DisplayGridContainer.propTypes = {
     type: React.PropTypes.string.isRequired,
     data: React.PropTypes.array.isRequired
