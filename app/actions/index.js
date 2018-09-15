3ee54916eca5ff55e53cfa1e8f85673a653ec18e
mlamont8 <mlamont8@hotmail.com>
2018-09-15 16:26:22 -0400

    give front page a random background image

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,99 +1,100 @@
-import fetchJsonp from 'fetch-jsonp';
+import fetchJsonp from "fetch-jsonp";
 
 // Fetching Front Page Data
 
-  export function fetchFrontData(){
-return (dispatch) => {
+export function fetchFrontData() {
+  return dispatch => {
     dispatch(frontIsLoading(true));
-    fetchJsonp('https://api.themoviedb.org/3/movie/upcoming/?api_key=21b0daca9dad79653c91d176b7930bee')
-        .then((response) => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            dispatch(frontIsLoading(false));
-            return response;
-        })
-        .then((response) => response.json())
-        .then((json) =>
-            dispatch(frontFetchDataSuccess(json.results)))
-        .catch(() => dispatch(frontHasErrored(true)));
+    fetchJsonp(
+      "https://api.themoviedb.org/3/movie/upcoming/?api_key=21b0daca9dad79653c91d176b7930bee"
+    )
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        dispatch(frontIsLoading(false));
+        return response;
+      })
+      .then(response => response.json())
+      .then(json => dispatch(frontFetchDataSuccess(json.results)))
+      .catch(() => dispatch(frontHasErrored(true)));
+  };
+}
 
-      }
-    }
-
-    export function frontFetchDataSuccess(items) {
-    return {
-        type: 'FRONT_FETCH_DATA_SUCCESS',
-        url: items[4].backdrop_path
-    };
+export function frontFetchDataSuccess(items) {
+  // get random number for random front page background
+  const randomNum = Math.floor(Math.random() * Math.floor(19));
+  return {
+    type: "FRONT_FETCH_DATA_SUCCESS",
+    url: items[randomNum].backdrop_path
+  };
 }
 
 export function frontHasErrored(bool) {
-  console.log('front has errored')
-    return {
-        type: 'FRONT_HAS_ERRORED',
-        hasErrored: bool
-    };
+  console.log("front has errored");
+  return {
+    type: "FRONT_HAS_ERRORED",
+    hasErrored: bool
+  };
 }
 
 export function frontIsLoading(bool) {
-    return {
-        type: 'FRONT_IS_LOADING',
-        isLoading: bool
-    };
+  return {
+    type: "FRONT_IS_LOADING",
+    isLoading: bool
+  };
 }
 
 // Fetching Search Term
 
 export function searchTerm(term) {
   return {
-    type: 'SEARCH_TERM_ENTERED',
+    type: "SEARCH_TERM_ENTERED",
     term
-  }
+  };
 }
 
-export function fetchExploreData(type, page){
-  console.log(type, page)
-return (dispatch) => {
-  dispatch(exploreRequest());
-  fetchJsonp('https://api.themoviedb.org/3/'+type+'/popular?api_key=21b0daca9dad79653c91d176b7930bee&language=en-US&page=' + page)
-      .then((response) => {
-          if (!response.ok) {
-              throw Error(response.statusText);
-          }
-        //   dispatch(exploreIsLoading(false));
-          return response;
-      })
-      .then((response) => response.json()
-      
+export function fetchExploreData(type, page) {
+  console.log(type, page);
+  return dispatch => {
+    dispatch(exploreRequest());
+    fetchJsonp(
+      "https://api.themoviedb.org/3/" +
+        type +
+        "/popular?api_key=21b0daca9dad79653c91d176b7930bee&language=en-US&page=" +
+        page
     )
-      .then((json) =>
-          dispatch(exploreFetchSuccess(json)))
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        //   dispatch(exploreIsLoading(false));
+        return response;
+      })
+      .then(response => response.json())
+      .then(json => dispatch(exploreFetchSuccess(json)))
       .catch(() => dispatch(exploreHasErrored(true)));
+  };
+}
 
-    }
-  }
-
-
-  export function exploreFetchSuccess(items) {
-      console.log(items)
+export function exploreFetchSuccess(items) {
+  console.log(items);
   return {
-      type: 'EXPLORE_FETCH_SUCCESS',
-      items
+    type: "EXPLORE_FETCH_SUCCESS",
+    items
   };
 }
 
 export function exploreHasErrored(bool) {
-    console.log('explore has errored')
-      return {
-          type: 'EXPLORE_HAS_ERRORED',
-          hasErrored: bool
-      };
-  }
-  
-  export function exploreRequest() {
-      return {
-          type: 'EXPLORE_REQUEST',
-          
-      };
-  }
+  console.log("explore has errored");
+  return {
+    type: "EXPLORE_HAS_ERRORED",
+    hasErrored: bool
+  };
+}
+
+export function exploreRequest() {
+  return {
+    type: "EXPLORE_REQUEST"
+  };
+}
